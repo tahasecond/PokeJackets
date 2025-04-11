e61a69cd26a58ec4fd3ad697d9e98196679b7e6f
DGTDT <50683138+DGTDT@users.noreply.github.com>
2025-04-10 21:54:12 -0400

    Made more changes to stats page

--- client/src/pages/PokemonStatsPage/PokemonStatsPage.jsx
+++ client/src/pages/PokemonStatsPage/PokemonStatsPage.jsx
@@ -1,17 +1,33 @@
 import './PokemonStatsPage.css';
 import { Link } from "react-router-dom";
+import pikapika from '../../assets/pikapika.png';
+
+
+// use process.env.POKEMON_API_KEY to use API key.
 
 const PokemonStatsPage = () => {
+    
   return (
     <div className="pokemon-page">
       <div className="header-container">
-        <div className="return-btn-container"><Link to="/">Back</Link></div>
+        <div className="return-btn-container">
+            <div id="btn"><Link to="/">Return</Link></div>
+            <div id="header">
+                <h3>PokeStats</h3>
+            </div>
+            <div id="subheader">
+                <p>Search the stats of all discoverable Pokemon!</p>
+            </div>
+            </div>
         <div className="search-bar"><input type="text" placeholder="Pikachu" /></div>
       </div>
 
       <div className="main-container">
-        <div className="image-container"></div>
-        <div className="stats-container"></div>
+        <div className="image-container">
+            <img src={pikapika} alt="Pokemon Image..." /></div>
+        <div className="stats-container">
+            <p id="result"></p>
+        </div>
       </div>
     </div>
   );
