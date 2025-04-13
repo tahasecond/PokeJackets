4affd73d34164f9698c05af1be21d79cce95d9e7
Adonai <adonaimehari823@gmail.com>
2025-04-12 22:16:48 -0400

    Update models.py
    Created Pokemon model

--- server/marketplace/models.py
+++ server/marketplace/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pokemon(models.py):
+    name = models.charField(max_length=100)
+    pokedex_number = models.IntegerField()
+    type_primary = models.CharField(max_length=50)
+    type_secondary = models.CharField(max_length=50, blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
